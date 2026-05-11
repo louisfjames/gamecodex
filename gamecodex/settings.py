d7f704fe0eddc4e756c1e7c6df000ac2c446e9b9
louisfjames <louisfjames@hotmail.co.uk>
2026-05-11 14:00:10 +0100

    feat: add base template and home page template

--- gamecodex/settings.py
+++ gamecodex/settings.py
@@ -28,7 +28,7 @@ from decouple import config
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'gamecodex.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
