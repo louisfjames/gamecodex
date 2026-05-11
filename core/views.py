be7c8ac37b4cef434179b2629876918f58aed78d
louisfjames <louisfjames@hotmail.co.uk>
2026-05-11 17:34:23 +0100

    feat: redirect authenticated users to profile and clean up home view

--- core/views.py
+++ core/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from django.shortcuts import render, redirect
 
 def home(request):
+    if request.user.is_authenticated:
+        return redirect('profile')
     return render(request, 'core/home.html')
