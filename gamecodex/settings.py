d2678379dc01ed5e6a428218e643a6e67f28d359
louisfjames <louisfjames@hotmail.co.uk>
2026-05-11 14:42:00 +0100

    feat: add footer with navigation links and social media icons

--- gamecodex/settings.py
+++ gamecodex/settings.py
@@ -28,7 +28,7 @@ from decouple import config
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
