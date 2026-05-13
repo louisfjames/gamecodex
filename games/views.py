f45d4bb9ecde32bef70d277b2c5eeb6b4dcb0ac0
louisfjames <louisfjames@hotmail.co.uk>
2026-05-13 13:54:27 +0100

    feat: add search_view to connect to search_games under games/views.py

--- games/views.py
+++ games/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .services.igdb import search_games
 
 def search_view(request):
-    return render(request, 'games/search.html')
+    query = request.GET.get("q", "")
+    response = search_games(query)
+
+    return render(request, "search.html", {
+        "query": query,
+        "error": response["error"],
+        "results": response["results"],
+    })
